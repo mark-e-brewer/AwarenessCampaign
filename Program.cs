e8b4d6ae4c9f8267fed98dc031638ee27e718316
Mark Brewer <markbrewer369@gmail.com>
2023-10-24 20:54:22 -0500

    post endpoint in progress

--- Program.cs
+++ Program.cs
@@ -53,10 +53,41 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/uservalidate/{uid}", (AwarenessCampaignDbContext db, string uid) =>
+{
+    var userExists = db.Users.Where(x => x.UID == uid).FirstOrDefault();
+    if (userExists == null)
+    {
+        return Results.StatusCode(204);
+    }
+    return Results.Ok(userExists);
+});
 
 app.MapGet("/users", (AwarenessCampaignDbContext db) =>
 {
     return db.Users.ToList();
+});
+
+app.MapGet("/posts", (AwarenessCampaignDbContext db) =>
+{
+    return db.Posts.ToList();
+});
+
+app.MapGet("/post/{id}", (AwarenessCampaignDbContext db, int id) => 
+{
+    var post = db.Posts.FirstOrDefault(p => p.Id == id);
+    if  (post == null) 
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(post);
+});
+
+app.MapPost("/post", async (AwarenessCampaignDbContext db, Post post) => 
+{
+    db.Posts.Add(post);
+    await db.SaveChangesAsync();
+    return Results.Created($"/post/{post.Id}", post);
 });
 
 app.Run();
