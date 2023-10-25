7df2cb74f529f13359946bf6aab830a399b9b224
Sean Bryant <smbryant1730@gmail.com>
2023-10-24 20:54:35 -0400

    created category endpoints and userById

--- Program.cs
+++ Program.cs
@@ -53,10 +53,87 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//User Endpoints
 
+//Get All Users
 app.MapGet("/users", (AwarenessCampaignDbContext db) =>
 {
     return db.Users.ToList();
+});
+
+// Get User by id
+app.MapGet("/api/users/{id}", (AwarenessCampaignDbContext db, int id) =>
+{
+    var user = db.Users.SingleOrDefault(u => u.Id == id);
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        return Results.Ok(user);
+    }
+});
+
+// Category Endpoints
+
+// Create Category
+app.MapPost("/api/category", (AwarenessCampaignDbContext db, Category category) =>
+{
+    db.Category.Add(category);
+    db.SaveChanges();
+    return Results.Created($"/api/categories/{category.Id}", category);
+});
+
+// Get All Categories
+app.MapGet("/api/categories", (AwarenessCampaignDbContext db) =>
+{
+    var categories = db.Category.ToList();
+    return Results.Ok(categories);
+});
+
+// Get Category by id
+app.MapGet("/api/categories/{id}", (AwarenessCampaignDbContext db, int id) =>
+{
+    var category = db.Category.SingleOrDefault(c => c.Id == id);
+    if (category == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        return Results.Ok(category);
+    }
+});
+
+// Update Category by id
+app.MapPut("/api/categories/{id}", (AwarenessCampaignDbContext db, int id, Category updatedCategory) =>
+{
+    var existingCategory = db.Category.SingleOrDefault(c => c.Id == id);
+    if (existingCategory == null)
+    {
+        return Results.NotFound();
+    }
+
+    existingCategory.Name = updatedCategory.Name;
+    // Update other properties as needed...
+
+    db.SaveChanges();
+    return Results.Ok();
+});
+
+// Delete Category by id
+app.MapDelete("/api/categories/{id}", (AwarenessCampaignDbContext db, int id) =>
+{
+    var category = db.Category.SingleOrDefault(c => c.Id == id);
+    if (category == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Category.Remove(category);
+    db.SaveChanges();
+    return Results.NoContent();
 });
 
 app.Run();
